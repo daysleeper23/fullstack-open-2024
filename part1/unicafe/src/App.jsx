6deada821c89e286a6566b68f36da86cb4cff484
daysleeper23 <charnel1986@gmail.com>
2024-09-03 21:46:17 +0300

    Ex 1.11
    - Display the statistics in an HTML table

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,9 +8,15 @@ const Header = ({title}) => {
 
 const StatisticsLine = ({text, value}) => {
   return (
-    <p>
-      {text} {value}
-    </p>
+    <tr>
+      <td>
+        {text}  
+      </td>
+      <td>
+        {value}
+      </td>
+    </tr>
+    
   )
 }
 
@@ -20,12 +26,14 @@ const Statistics = ({good, neutral, bad}) => {
     return (  
       <>
         <Header title='statistics' />
-        <StatisticsLine text='good' value={good} />
-        <StatisticsLine text='neutral' value={neutral} />
-        <StatisticsLine text='bad' value={bad} />
-        <StatisticsLine text='all' value={good + neutral + bad} />
-        <StatisticsLine text='average' value={(good * 1 + neutral * 0 + bad * (-1)) / (good + neutral + bad)} />
-        <StatisticsLine text='positive' value={good / (good + neutral + bad) * 100} />
+        <table>
+          <StatisticsLine text='good' value={good} />
+          <StatisticsLine text='neutral' value={neutral} />
+          <StatisticsLine text='bad' value={bad} />
+          <StatisticsLine text='all' value={good + neutral + bad} />
+          <StatisticsLine text='average' value={(good * 1 + neutral * 0 + bad * (-1)) / (good + neutral + bad)} />
+          <StatisticsLine text='positive' value={good / (good + neutral + bad) * 100} />
+        </table>
       </>
     )
   }
