a4c7f32a1605ee88a02ee63db8f195d1ede7da89
daysleeper23 <charnel1986@gmail.com>
2024-09-05 11:02:22 +0300

    Ex 3.1
    Phonebook backend step 1
    - Implement a Node application that returns a hardcoded list of phonebook entries from the address http://localhost:3001/api/persons
    
    - The application must be started with the command npm start.
    
    - The application must also offer an npm run dev command that will run the application and restart the server whenever changes are made and saved to a file in the source code

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const jsonData = require('./db.json')
+
+let persons = jsonData.persons
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
