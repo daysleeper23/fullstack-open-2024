75e772b2e1749dff421a4d798ca03c003739db76
daysleeper23 <charnel1986@gmail.com>
2024-09-06 00:13:20 +0300

    Ex 3.14
    - Extract all Mongoose-specific code into its own module
    - Change the fetching of all phonebook entries so that the data is fetched from the database.
    - Change the backend so that new numbers are saved to the database

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const jsonData = require('./db.json')
+const Person = require('./models/person')
 const cors = require('cors')
-
 
 //logging configuration
 var morgan = require('morgan')
@@ -18,8 +18,6 @@ app.use(express.json())
 app.use(express.static('dist'))
 app.use(logger)
 
-let persons = jsonData.persons
-
 //generic
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -28,38 +26,47 @@ app.get('/', (request, response) => {
 //get info
 app.get('/info', (request, response) => {
   const date = new Date()
-  response.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${date}</p>
-  `)
+  Person
+    .find({})
+    .then(persons => {
+      response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${date}</p>
+      `)
+    })
+    .catch(error => {
+      return response.status(400).json({ 
+        error: error.message
+      })
+    })
 })
 
 //get all persons
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person
+    .find({})
+    .then(persons => {
+      response.json(persons)
+    })
+    .catch(error => {
+      return response.status(400).json({ 
+        error: error.message
+      })
+    })
 })
 
 //get single person
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
 
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).send('Sorry, person not found')
-  }
+  Person
+    .findById(request.params.id)
+    .then(person => {
+      response.json(person)
+    })
+    .catch(error => {
+      return response.status(404).send('Sorry, person not found')
+    })
 })
-
-//generate ID for new person
-const generateId = () => {
-  const newId = Math.floor(Math.random() * 10000) + 1
-
-  while (persons.findIndex(person => person.id === newId) !== -1)
-    newId = Math.floor(Math.random() * 10000) + 1
-  
-  return newId.toString()
-}
 
 //create new person
 app.post('/api/persons', (request, response) => {  
@@ -71,35 +78,39 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  if (persons.findIndex(person => person.name === body.name) !== -1)
-    return response.status(400).json({ 
-      error: 'name must be unique' 
-    })
+  // if (persons.findIndex(person => person.name === body.name) !== -1)
+  //   return response.status(400).json({ 
+  //     error: 'name must be unique' 
+  //   })
 
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  }
-
-  persons = persons.concat(person)
-
-  response.json(person)
+  })
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => {
+      return response.status(400).json({
+        error: error.message
+      })
+    })
 })
 
 //delete person
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id.toString()
-  const beforeDeleteCount = persons.length
-  persons = persons.filter(person => person.id !== id)
+// app.delete('/api/persons/:id', (request, response) => {
+//   const id = request.params.id.toString()
+//   const beforeDeleteCount = persons.length
+//   persons = persons.filter(person => person.id !== id)
   
-  if (persons.length < beforeDeleteCount)
-    response.status(204).end()
-  else
-    response.status(404).send('Sorry, person not found')
-})
+//   if (persons.length < beforeDeleteCount)
+//     response.status(204).end()
+//   else
+//     response.status(404).send('Sorry, person not found')
+// })
 
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
