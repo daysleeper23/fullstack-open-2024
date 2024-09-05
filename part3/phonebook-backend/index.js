6d830b534433a842e553829e3e3186c217c68441
daysleeper23 <charnel1986@gmail.com>
2024-09-05 11:41:03 +0300

    Ex 3.4
    Implement functionality that makes it possible to delete a single phonebook entry by making an HTTP DELETE request to the unique URL of that phonebook entry.

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -4,10 +4,12 @@ const jsonData = require('./db.json')
 
 let persons = jsonData.persons
 
+//generic
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
+//info
 app.get('/info', (request, response) => {
   const date = new Date()
   response.send(`
@@ -16,10 +18,12 @@ app.get('/info', (request, response) => {
   `)
 })
 
+//all persons
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+//single person
 app.get('/api/persons/:id', (request, response) => {
   const id = request.params.id
   const person = persons.find(person => person.id === id)
@@ -29,6 +33,18 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).send('Sorry, person not found')
   }
+})
+
+//delete person
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id.toString()
+  const beforeDeleteCount = persons.length
+  persons = persons.filter(person => person.id !== id)
+  
+  if (persons.length < beforeDeleteCount)
+    response.status(204).end()
+  else
+    response.status(404).send('Sorry, person not found')
 })
 
 const PORT = 3001
