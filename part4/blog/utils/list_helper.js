4097777a9383c3977044dff4cf1d482321f3a17e
daysleeper23 <charnel1986@gmail.com>
2024-09-06 14:08:01 +0300

    Ex 4.4 4.5
    - Define totalLikes: function that receives a list of blog posts, returns the total sum of likes
    - Define favoriteBlog: function that receives a list of blogs, return the blog has the most likes. If there are many top favorites, it is enough to return one of them.

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,8 +1,31 @@
-const dummy = (blogs) => {
+const blog = require("../models/blog")
+
+const totalLikes = (blogs) => {
   // ...
-  return 1
+  const totalLikes = blogs.reduce((sum, blog) => sum = sum + blog.likes, 0)
+  return blogs.length == 0
+    ? 0
+    : totalLikes
+}
+
+const favoriteBlog = (blogs) => {
+
+  if (blogs.length == 0)
+    return {}
+
+  const likes = blogs.map(blog => blog.likes)
+  const topLikes = Math.max(...likes)
+  const favoriteIndex = likes.indexOf(topLikes)
+  const topBlog = {
+    title: blogs[favoriteIndex].title,
+    author: blogs[favoriteIndex].author,
+    likes: blogs[favoriteIndex].likes
+  }
+  
+  return topBlog
 }
 
 module.exports = {
-  dummy
+  totalLikes,
+  favoriteBlog
 }
