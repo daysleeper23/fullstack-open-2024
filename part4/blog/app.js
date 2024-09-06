9c5f8e7c13c6a7998683cb148562b30fb88f3077
daysleeper23 <charnel1986@gmail.com>
2024-09-06 10:29:58 +0300

    Ex 4.2
    - Refactoring middleware: unknownEndpoint, requestLogger & errorHandler

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -1,15 +1,14 @@
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
 const express = require('express')
+const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const app = express()
-
-const config = require('./utils/config')
-const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB')
   })
@@ -19,6 +18,11 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
