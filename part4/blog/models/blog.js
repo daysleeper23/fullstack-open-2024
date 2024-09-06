c9c3783336b92cbe07c3b9761baca6a35b320993
daysleeper23 <charnel1986@gmail.com>
2024-09-06 09:55:37 +0300

    Ex 4.2
    - Refactoring app from index.js
    - Moving DBconnect to app from model

--- part4/blog/models/blog.js
+++ part4/blog/models/blog.js
@@ -1,14 +1,4 @@
 const mongoose = require('mongoose')
-const config = require('../utils/config')
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB', error.message)
-  })
 
 const blogSchema = new mongoose.Schema({
   title: String,
