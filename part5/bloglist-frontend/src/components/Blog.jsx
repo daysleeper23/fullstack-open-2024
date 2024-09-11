4b45bc2de8aec141f4f7f0dfa09fbc43edec4b72
daysleeper23 <charnel1986@gmail.com>
2024-09-12 02:43:25 +0300

    Ex 5.7
    Show/hide button to toggle full details of a blog

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,32 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [show, setShow] = useState(false)
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={() => setShow(!show)}>{show ? 'hide' : 'show'}</button>
+      {show
+        ? 
+          <>
+            <div>{blog.url}</div>
+            likes {blog.likes} 
+            <button>like</button>
+            <div>{blog.user.name}</div>
+          </>
+        : <></>
+      }
+    </div> 
+  )
+}
 
 export default Blog
