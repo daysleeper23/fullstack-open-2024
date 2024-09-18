e5c353660c278a1cc86d7a5a64bfd7555d01a8ab
daysleeper23 <charnel1986@gmail.com>
2024-09-18 12:16:19 +0300

    Ex 7.5 7.6
    - Add a button to the form to clear all the input fields.
    - Destructuring reset from the hook's return to avoid warning.

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -2,14 +2,10 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useField } from '../hooks'
 
-const CreateNew = (props) => {
-  // const [content, setContent] = useState('')
-  // const [author, setAuthor] = useState('')
-  // const [info, setInfo] = useState('')
-  
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
+const CreateNew = (props) => {  
+  const { reset: resetContent, ...content } = useField('text')
+  const { reset: resetAuthor, ...author } = useField('text')
+  const { reset: resetInfo, ...info } = useField('text')
 
   const navigate = useNavigate()
 
@@ -24,23 +20,32 @@ const CreateNew = (props) => {
     navigate('/')
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+    resetContent()
+    resetAuthor()
+    resetInfo()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' {...content} />
+          <input required name='content' {...content} />
         </div>
         <div>
           author
-          <input name='author' {...author} />
+          <input required name='author' {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' {...info} />
+          <input required name='info' {...info} />
         </div>
-        <button>create</button>
+
+        <button type='submit'>create</button>
+        <button type='reset' onClick={handleReset}>reset</button>
       </form>
     </div>
   )
