de6cecc329bec97e0db51cbc192a3c002d125906
daysleeper23 <charnel1986@gmail.com>
2024-09-25 00:49:06 +0300

    Ex 9.2
    Calculate:
    - the number of days
    - the number of training days
    - the original target value
    - the calculated average time
    - boolean value describing if the target was reached
    - a rating between the numbers 1-3 that tells how well the hours are met.
    - a text value explaining the rating
    
    Neutralizing edge cases:
    - empty training data
    - negative training hours
    - negative or 0 target

---
+++ part9/bmi-calc/exerciseCalculator.ts
@@ -0,0 +1,82 @@
+interface Result {
+  periodLength: number,
+  trainingDays: number,
+  success: boolean,
+  rating: number,
+  ratingDescription: string,
+  target: number,
+  average: number
+}
+
+const exerciseCalculator = (exerciseHours: number[], target: number): Result => {
+  const periodLength = exerciseHours.length
+  console.log('p length', periodLength)
+  if (periodLength === 0)
+  {
+    return {
+      periodLength,
+      trainingDays: 0,
+      success: false,
+      rating: 0,
+      ratingDescription: "invalid data - zero training",
+      target,
+      average: 0
+    }
+  }
+
+  const dataValidation = exerciseHours.filter(hour => hour < 0).length
+  if (dataValidation > 0)
+  {
+    return {
+      periodLength,
+      trainingDays: 0,
+      success: false,
+      rating: 0,
+      ratingDescription: "invalid data - negative hours",
+      target,
+      average: 0
+    }
+  }
+
+  const trainingDays = exerciseHours.filter(hour => hour > 0).length
+  const average = exerciseHours.reduce((acc, cur) => acc = acc + cur) / periodLength
+  const success = target > average ? false : true
+  let rating = 0
+  let ratingDescription = "not too bad but could be better"
+
+  if (target > 0) {
+    const performance = average / target * 100
+    console.log('performance', performance)
+    switch (true) {
+      case (performance < 75):
+        rating = 1
+        ratingDescription = "definitely need more effort"
+        break
+      case (performance < 98):
+        rating = 2
+        ratingDescription = "not too bad but could be better"
+        break
+      default:
+        rating = 3
+        ratingDescription = "keep up the good work"
+        break
+    }
+  } else {
+    console.log("invalid target")
+    rating = 0
+    ratingDescription = "invalid data - target"
+  }
+    
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average
+  }
+}
+
+console.log(exerciseCalculator([3, 0, 2, 4.5, 0, 3, 1], 2))
